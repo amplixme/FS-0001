02d10374e4c3e7c6d378255277195ec214493ef2
Brian Acuña <brian2012briga@gmail.com>
2026-05-09 01:14:22 -0300

    fix: correct token path in localStorage

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -40,7 +40,7 @@ function Login() {
         password: data.password,
       });
 
-      localStorage.setItem('token', response.data.token);
+      localStorage.setItem('token', response.data.data.token);
 
       navigate('/');
     } catch (error) {
