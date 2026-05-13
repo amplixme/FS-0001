ca9da9926a53044b1475530a100feb8e2bd8cfa6
Baut1 <bauti.iglesias@gmail.com>
2026-05-13 10:03:27 -0300

    feat(posts): add routes for fetching all posts and a post by ID

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const router = Router();
 
-const { create } = require('../controllers/post.controller');
+const { create, getAll, getById } = require('../controllers/post.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 const validate = require('../middlewares/validate.middleware');
 const { createPostSchema } = require('../utils/schemas/post.schema');
 
 router.post('/', authMiddleware, validate(createPostSchema), create);
+router.get('/', getAll);
+router.get('/:id', getById);
 
 module.exports = router;
