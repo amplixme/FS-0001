3223e923ee6361862f5ff4fe179f6df0f5c32f41
NicolasArtemio <nicolasartemiodume@gmail.com>
2026-05-14 14:51:17 -0300

    feat(posts): add post detail page (#28)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import Layout from './components/layout/Layout'
 import ProtectedRoute from './components/ProtectedRoute'
+import PostDetail from './pages/PostDetail'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/posts/:id" element={<PostDetail />} />
           </Routes>
         </Layout>
       </AuthProvider>
